f12e409f02c81cb7d46fd3dbad33e9abc6b7ad18
Musa Zhou <musazhou@gmail.com>
2018-08-29 22:36:34 +0800

    sigin template

--- muslim_app_bk/muslimappbk/muslimappbk/urls.py
+++ muslim_app_bk/muslimappbk/muslimappbk/urls.py
@@ -17,12 +17,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from management import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('management/', include('management.urls'))
+    path('management/', include('management.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# accounts/login/ [name='login']
+# accounts/logout/ [name='logout']
+# accounts/password_change/ [name='password_change']
+# accounts/password_change/done/ [name='password_change_done']
+# accounts/password_reset/ [name='password_reset']
+# accounts/password_reset/done/ [name='password_reset_done']
+# accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/reset/done/ [name='password_reset_complete']
