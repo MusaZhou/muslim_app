ef05edbd6e2c2e128cf6f03d34f295d538e7d481
Musa Zhou <musazhou@gmail.com>
2018-08-29 18:35:08 +0800

    manage the static files

---
+++ muslim_app_bk/muslimappbk/management/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.conf import settings
+from . import views
+
+app_name = 'management'
+
+urlpatterns = [
+    path('404', views.page_404, name='404'),
+    path('500', views.page_500, name='500'),
+    path('datatable', views.datatable, name='datatable'),
+    path('forms', views.forms, name='forms'),
+    path('<str:characters>', views.others, name='others')
+]
--- muslim_app_bk/muslimappbk/management/views.py
+++ muslim_app_bk/muslimappbk/management/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 # Create your views here.
+def page_404(request):
+    return render(request, 'management/404.html')
+
+def page_500(request):
+    return render(request, 'management/500.html')
+
+def datatable(request):
+    return render(request, 'management/datatable.html')
+
+def forms(request):
+    return render(request, 'management/forms.html')
+
+def others(request, characters):
+    print('what is requesting is :' + characters)
+    return redirect(settings.STATIC_URL + request.resolver_match.namespace + '/' + characters)
