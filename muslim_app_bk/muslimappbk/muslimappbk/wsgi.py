22e383792db30fa7e59af2f21e7d1d48c7d8b878
Musa Zhou <musazhou@gmail.com>
2018-09-20 17:52:13 +0800

    take out path setting from wsgi to the server config

--- muslim_app_bk/muslimappbk/muslimappbk/wsgi.py
+++ muslim_app_bk/muslimappbk/muslimappbk/wsgi.py
@@ -11,9 +11,10 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if path not in sys.path:
-    sys.path.append(path)
+# If path has not been set in the server config, need append here
+# path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# if path not in sys.path:
+#     sys.path.append(path)
     
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muslimappbk.settings')
 
