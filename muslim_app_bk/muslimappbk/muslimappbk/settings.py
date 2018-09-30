e6b2660ee4799824bd9949d626d7e61f836049c6
Musa Zhou <musazhou@gmail.com>
2018-09-30 18:34:55 +0800

    start i18n

--- muslim_app_bk/muslimappbk/muslimappbk/settings.py
+++ muslim_app_bk/muslimappbk/muslimappbk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,6 +147,15 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = [
+    ('zh', _('Chinese')),
+    ('en', _('English')),
+]
+
+LOCALE_PATHS = [
+        os.path.join(BASE_DIR, 'local')
+    ]
 
 TIME_ZONE = 'Asia/Shanghai'
 
