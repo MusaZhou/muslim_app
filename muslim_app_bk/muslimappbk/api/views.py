12dfd94324478b0091d2dceddd5551f1f6645c80
Musa Zhou <musazhou@gmail.com>
2018-10-08 13:54:47 +0800

    add pagination to index page

--- muslim_app_bk/muslimappbk/api/views.py
+++ muslim_app_bk/muslimappbk/api/views.py
@@ -9,15 +9,26 @@ from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.http import http_date
 import hashlib
+from django.core.paginator import InvalidPage, Paginator
 
 class AppListView(generics.ListAPIView):
     serializer_class = AppSerializer
     
     def get_queryset(self):
         category_id = int(self.request.query_params.get('cate_id', 9999))
+        page = int(self.request.query_params.get('page', 1))
+        
         if category_id == 9999:
-            return MobileApp.shown_apps.all()
-        return MobileApp.shown_apps.filter(category__id=category_id)
+            mobile_list_all = MobileApp.shown_apps.all()
+        else:    
+            mobile_list_all = MobileApp.shown_apps.filter(category__id=category_id)
+            
+        paginator = Paginator(mobile_list_all, 9)
+        try:
+            mobile_list = paginator.page(page)
+            return mobile_list.object_list
+        except InvalidPage:
+            return MobileApp.objects.none()
     
 def app_download_count(request):
     app_slug = request.GET['app_slug']
